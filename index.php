bb7a09a70a0463b06d8d47bb7da940b4ddaef22a
Ctmax-ui <debjeetbiswas121@gmail.com>
2024-02-13 10:24:14 +0530

    fixed some bug

--- index.php
+++ index.php
@@ -198,29 +198,6 @@ if (isset($_POST["submitpost"])) {
     </div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="./js/index.js"></script>
 </body>
